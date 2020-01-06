4a3eb55c53b94cc7e0ba612b56b37102d6b587ab
Myke Bates <contact@mykebates.com>
2020-01-06 06:32:11 -0600

    comitting progress

--- web/js/main.js
+++ web/js/main.js
@@ -1,15 +1,33 @@
 $(function() {
     $('#subscribe').on('submit', function (e) {
         var name = $('#name'),
-            email = $('#email');
+            email = $('#email'),
+            form = $(this),
+            nameVal = name.val(),
+            emailVal = email.val()
 
-        swal('Excellent!', 'Thanks, ' + name.value + '. Method Conf news will be headed your way soon.', 'success');
+        if(validateEmail(email.val()))
+        {
+            $.post(form.attr('action'), form.serialize()).done(function( data ) {
+                console.log(data);
+                name.val('');
+                email.val('');
 
-        name.val('');
-        email.val('');
+                swal('Excellent!', 'Thanks, ' + nameVal + '. Method Conf news will be headed your way soon.', 'success');
+            });
+
+        } else {
+            swal('Whoops', 'Something went wrong... Please double check that your email address is valid. And if so, yell at us on the Twitter and tell us our stuff is broken.', 'error');
+        }
 
         e.preventDefault();
     });
+
+    function validateEmail(email)
+    {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
     
     $('.more').on('click', function(){
         var parent = $(this).closest('.speaker_card');
@@ -53,4 +71,60 @@ $(function() {
         
         return false;
     });
+
+    $('.image_slides ol li').on('click', function(){
+        setImage($(this).index());
+    });
+
+    $('.image_slides i').on('click', function(){
+        var direction = $(this).data('direction'),
+            totalSlides = $('.image_slides ol li').length,
+            currentSlide = $('.image_slides ol li.current').index(),
+            nextIndex = 0;
+
+        
+
+        if(direction == 'next') {
+            if(currentSlide + 1 != totalSlides) {
+                nextIndex = currentSlide + 1;
+            }
+        } else {
+            if(currentSlide == 0) {
+                nextIndex = totalSlides - 1;
+            } else {
+                nextIndex = currentSlide - 1;
+            }
+        }
+        
+        setImage(nextIndex);
+    });
+
+    function setImage(index) {
+        var imageSlides = $('.image_slides');
+
+        $('.images img:visible', imageSlides).removeClass('current');
+        $('.images img:eq('+ index +')', imageSlides).addClass('current')
+        $('li', imageSlides).removeClass('current');
+        $('li:eq('+ index +')', imageSlides).addClass('current');
+    }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
